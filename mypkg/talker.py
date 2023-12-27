5f733011851b2c687a06520bbb82a57acf4ebd29
saawaa <saawaa0050@gmail.com>
2023-12-28 06:16:22 +0900

    change talker class

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -2,17 +2,20 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int16
 
+class Talker():
+    def __int__(self):
+        self.pub = node.create_publisher(Int16,"countup",
+        self.n = 0)
+
 rclpy.init()
 node = Node("talker")
-pub = node.create_publisher(Int16, "countup", 10)
-n = 0
+talker = Talker()
 
 def cb():
-    global n
     msg = Int16()
-    msg.data = n
-    pub.publish(msg)
-    n += 1
+    msg.data = talaker.n
+    talker.pub.publish(msg)
+    talker.n += 1
 
-node.create_timer(0.5, cb)
+node.create_timer(0.5, cd)
 rclpy.spin(node)
