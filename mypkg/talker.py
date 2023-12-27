fd6960ebe21bc0a7896abe6ef0afdb11dcaaeef0
saawaa <saawaa0050@gmail.com>
2023-12-28 04:02:43 +0900

    talker listener fi

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -4,15 +4,15 @@ from std_msgs.msg import Int16
 
 rclpy.init()
 node = Node("talker")
-pub = node.create_publisher(Int16,"countup",10)
+pub = node.create_publisher(Int16, "countup", 10)
 n = 0
 
-def cd():
+def cb():
     global n
     msg = Int16()
     msg.data = n
     pub.publish(msg)
     n += 1
 
-node.create_timer(0.5,cd)
+node.create_timer(0.5, cb)
 rclpy.spin(node)
